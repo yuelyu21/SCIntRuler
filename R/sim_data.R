ddeb633d9674ab6bd7d76a407451c7e7f4622c52
yuelyu21 <yuelyu0521@gmail.com>
2024-01-17 00:29:11 -0600

    Upload with example

---
+++ R/sim_data.R
@@ -0,0 +1,10 @@
+#' My Example Dataset
+#'
+#' A short description of your dataset.
+#'
+#' @format An example PBMC data with Seurat format
+#' \describe{
+#'   \item{assays}{Count matrix}
+#'   \item{meta.data}{Meta data for each cell}
+#' }
+"sim_data"
