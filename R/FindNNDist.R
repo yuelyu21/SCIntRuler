f985ed70e29e25f2ade1641ccaef3e7209439028
yuelyu21 <yuelyu0521@gmail.com>
2024-01-18 04:53:58 -0600

    Upload with example

--- R/FindNNDist.R
+++ R/FindNNDist.R
@@ -20,26 +20,34 @@
 #' }
 
 
-
 FindNNDist <- function(fullcluster,normCount, meaningn = 20) {
+  #### find nearest neighbour
 
+  # allksP <- list()
   nclust <- rep(NA, length(fullcluster))
 
   for(i in 1:length(fullcluster)) {
     nclust[i] <- max(fullcluster[[i]]$finecluster)
   }
 
+
+  # allrevDiff <- matrix(NA, max(nclust), length(fullcluster))
+
+
   dist_mat <- list()
 
   for(j in 1:length(fullcluster)) {
     cat("j = ", j,"; ")
 
-    onecount <- normCount[[j]]
-    onemeta <- fullcluster[[j]]
+    onecount <- normCount[[j]] # 9763 x 2400
+    onemeta <- fullcluster[[j]] # 2400 * 3
 
     tmpvec <- c()
     newcounter = 1
 
+    # when j = 1, the dimension of othercount is 9763 3500 study 3+ study 2
+    # when j = 2, the dimension of othercount is 9763 4800 study 1+ study 3
+    # when j = 3, the dimension of othercount is 9763 4800 study 2+ study 3
     for(qqq in 1:length(fullcluster)) {
       if(qqq != j) {
         if(newcounter == 1) {
@@ -56,8 +64,12 @@ FindNNDist <- function(fullcluster,normCount, meaningn = 20) {
 
     onep_internal = onep_external = list()
 
+    # revDiff <- rep(NA, nclust[j])
+    # onep_vec <- rep(NA, length(allCT))
+
 
     for(q in 1:length(allCT)) {
+      # print(q)
 
       ct = allCT[q]
 
@@ -65,35 +77,49 @@ FindNNDist <- function(fullcluster,normCount, meaningn = 20) {
 
       max1 <- sum(onemeta$finecluster == ct)
       max2 <- sum(onemeta$finecluster != ct)
+      #！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+      # subc is the random 10 cells from this cluster; otherc is the ramdom 500 cells in other clusters
+      subc <- onecount[, sample(which(onemeta$finecluster == ct), min(20, max1))] # dim 9763 * 10
+      otherc <- onecount[, sample(which(onemeta$finecluster != ct), min(500, max2))] # dim 9763 * 500
+      # onecount 9763 x 2400 othercount 9763 * 3500
 
-      subc <- onecount[, sample(which(onemeta$finecluster == ct), min(20, max1))]
-      otherc <- onecount[, sample(which(onemeta$finecluster != ct), min(500, max2))]
-
-
+      # otherc_external 9763 * 1000
       otherc_external <- as.matrix(othercount[,sample(1:ncol(othercount), min(500*length(normCount[-j]), ncol(othercount))) ])
+      #                                        sample(1: 3500           , min(500* 2                   ,   3500.   )
 
+      for(nnn in 1:ncol(subc)) {
+        ## tmp dim 1 * 500
+        tmp <- sort(sqrt(Matrix::colSums(sweep(otherc, 1, subc[,nnn], "-")^2)))[1:meaningn]
+        onecdist_internal[[nnn]] <- tmp
 
-      internal_distmat <- crossdist(t(as.matrix(otherc)),t(as.matrix(subc)))
+        ## tmp1 dim 1 * 1000
+        tmp1 <- sort(sqrt(Matrix::colSums(sweep(otherc_external, 1, subc[,nnn], "-")^2)))[1:meaningn]
+        onecdist_external[[nnn]] <- tmp1
+      }
 
-      onecdist_internal <- apply(internal_distmat,2,sort)[1:meaningn,]
+      # convert to a dataframe is better
+      # dist1 <- onecdist_internal
+      # dist2 <- onecdist_external
 
-      external_distmat <- crossdist(t(as.matrix(otherc_external)),t(as.matrix(subc)))
+      onep_internal[[q]] <- onecdist_internal
+      onep_external[[q]] <- onecdist_external
+      # names(onep_internal) <- paste0("Cluster-",seq(1:q),"-dist1")
+      # names(onep_external) <- paste0("Cluster-",seq(1:q),"-dist2")
+      # revDiff[q] <- (mean(dist2) - mean(dist1))/max(mean(dist2),mean(dist1))
 
-      onecdist_external <- apply(external_distmat,2,sort)[1:meaningn,]
-
-
-      onep_internal[[q]] <- as.list(data.frame(onecdist_internal))
-      onep_external[[q]] <- as.list(data.frame(onecdist_external))
-
+      # ksout <- ks.test(dist1, dist2)
+      # ksout <- t.test(dist1, dist2)
+      # onep_vec[q] <- ksout$p.value
     }
     names(onep_internal) <- paste0("Cluster-",seq(1:length(allCT)),"-dist1")
     names(onep_external) <- paste0("Cluster-",seq(1:length(allCT)),"-dist2")
 
     dist_mat[[j]] <- list(onep_internal,onep_external)
 
+    #allrevDiff[1:nclust[j], j] <- revDiff
+    # allksP[[j]] <- onep_vec
   }
 
   names(dist_mat) <- paste0("sample-",seq(1:length(fullcluster)))
   return(dist_mat)
 }
-
