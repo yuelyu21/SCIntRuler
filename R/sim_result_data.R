638e537da88d5c79196001bc852269ea43b651ee
yuelyu21 <yuelyu0521@gmail.com>
2024-02-08 14:56:24 -0600

    upload an example data

--- R/sim_result_data.R
+++ R/sim_result_data.R
@@ -7,6 +7,7 @@
 #'   \item{sim_result[[1]]}{fullcluster}
 #'   \item{sim_result[[2]]}{normCount}
 #'   \item{sim_result[[3]]}{distmat}
+#'   \item{sim_result[[4]]}{testres}
 #' }
 #' @name sim_result
 "sim_result"
