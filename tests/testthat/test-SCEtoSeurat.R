89e18e97c382b69204868e35974db4861fc31005
yuelyu21 <yuelyu0521@gmail.com>
2024-05-08 03:33:08 -0500

    test package

---
+++ tests/testthat/test-SCEtoSeurat.R
@@ -0,0 +1,9 @@
+library(S4Vectors)
+test_that("SCEtoSeurat", {
+  data("sim_data_sce")
+
+  seurat_obj <- SCEtoSeurat(sim_data_sce)
+  # expect_true(inherits(seurat_obj, "Seurat"))
+  expect_equal(as.matrix(SummarizedExperiment::assay(sim_data_sce, "counts")), as.matrix(seurat_obj[["RNA"]]@counts))
+
+})
